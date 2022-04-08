6488b81f58ddf46d55e9de696b327f9bd68ae83b
andrewbelleza <54964125+andrewbelleza@users.noreply.github.com>
2022-04-08 08:46:01 +0800

    Update Win1021H2-Pro.ps1

--- Win1021H2-Pro.ps1
+++ Win1021H2-Pro.ps1
@@ -1,37 +1,57 @@
 Write-Host  -ForegroundColor Green "Starting ZeroTouch Cloud Imaging"
 Start-Sleep -Seconds 5
 
-#Change Display Resolution for Virtual Machine
+# Change Display Resolution for Virtual Machine
 if ((Get-MyComputerModel) -match 'Virtual') {
     Write-Host  -ForegroundColor Green "Setting Display Resolution to 1600x"
     Set-DisRes 1600
 }
 
-#Make sure I have the latest OSD Content
+# Make sure I have the latest OSD Content
 Write-Host  -ForegroundColor Green "Updating OSD PowerShell Module"
 Install-Module OSD -Force
 
 Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
 Import-Module OSD -Force
 
-#Start OSDCloud ZTI the RIGHT way
-Write-Host  -ForegroundColor Green "Start OSDCloud"
-Start-OSDCloud -OSLanguage en-us -OSBuild "21H2" -OSEdition Pro -ZTI
+# Start-OSDCloud with Params
+Write-Host  -ForegroundColor Green "Installing Windows 10 Pro 21H2 Operating System"
+$Params = @{
+    OSBuild = "21H2"
+    OSEdition = "Pro"
+    OSLanguage = "en-us"
+    ZTI = $True
+}
+Start-OSDCloud @Params
 
-#For Testing
-#Write-Host  -ForegroundColor Cyan "Start OSD Windows 10 21H2 with Firmware Update"
-#Start-OSDCloud -OSLanguage de-de -OSVersion 'Windows 10' -OSBuild 21H2 -OSEdition Pro -Firmware -ZTI
+# OOBEDeploy Bloatware Removal, Update Windows & Drivers
+Write-Host  -ForegroundColor Green "Removing Bloatware and Updating Windows & Drivers"
+$Params = @{
+    RemoveAppx = "Skype","Solitaire","Xbox","ZuneMusic","ZuneVideo"
+    UpdateDrivers = $true
+    UpdateWindows = $true
+}
+Start-OOBEDeploy @Params
 
-#Some Bloatware apps removal
-Get-AppxPackage *xboxapp* | Remove-AppxPackage
-Get-AppxPackage *zunevideo* | Remove-AppxPackage
-Get-AppxPackage *solitairecollection* | Remove-AppxPackage
-Get-AppxPackage *zunemusic* | Remove-AppxPackage
-Get-AppxPackage Microsoft.MixedReality.Portal | Remove-AppxPackage
-Get-AppxPackage Microsoft.XboxGamingOverlay | Remove-AppxPackage
+# Set OOBEDeploy CMD.ps1
+$SetCommand = @'
+@echo off
+:: Set the PowerShell Execution Policy
+PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
+:: Add PowerShell Scripts to the Path
+set path=%path%;C:\Program Files\WindowsPowerShell\Scripts
+:: Open and Minimize a PowerShell instance just in case
+start PowerShell -NoL -W Mi
+:: Install the latest OSD Module
+start "Install-Module OSD" /wait PowerShell -NoL -C Install-Module OSD -Force -Verbose
+:: Start-OOBEDeploy
+:: The next line assumes that you have a configuration saved in C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json
+start "Start-OOBEDeploy" PowerShell -NoL -C Start-OOBEDeploy
+exit
+'@
+$SetCommand | Out-File -FilePath "C:\Windows\OOBEDeploy.cmd" -Encoding ascii -Force
 
-
-#Restart from WinPE
+# Restart from WinPE
 Write-Host  -ForegroundColor Green "Restarting in 10 seconds!"
 Start-Sleep -Seconds 10
 wpeutil reboot
