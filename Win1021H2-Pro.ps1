211c6f1e1ff01e229473058701ea8de8ce0f51ab
andrewbelleza <54964125+andrewbelleza@users.noreply.github.com>
2022-06-02 07:33:52 +0800

    Update Win1021H2-Pro.ps1

--- Win1021H2-Pro.ps1
+++ Win1021H2-Pro.ps1
@@ -16,20 +16,7 @@ Import-Module OSD -Force
 
 #Start OSDCloud ZTI the RIGHT way
 Write-Host  -ForegroundColor Green "Start OSDCloud"
-Start-OSDCloud -OSLanguage en-us -OSBuild "21H2" -OSEdition Pro -ZTI
-
-#For Testing
-#Write-Host  -ForegroundColor Cyan "Start OSD Windows 10 21H2 with Firmware Update"
-#Start-OSDCloud -OSLanguage de-de -OSVersion 'Windows 10' -OSBuild 21H2 -OSEdition Pro -Firmware -ZTI
-
-<#Some Bloatware apps removal
-Get-AppxPackage *xboxapp* | Remove-AppxPackage
-Get-AppxPackage *zunevideo* | Remove-AppxPackage
-Get-AppxPackage *solitairecollection* | Remove-AppxPackage
-Get-AppxPackage *zunemusic* | Remove-AppxPackage
-Get-AppxPackage Microsoft.MixedReality.Portal | Remove-AppxPackage
-Get-AppxPackage Microsoft.XboxGamingOverlay | Remove-AppxPackage#>
-
+Start-OSDCloud -OSVersion 'Windows 10' -OSBuild 21H2 -OSEdition Pro -OSLanguage en-us -OSLicense Volume -Firmware -ZTI
 
 #Restart from WinPE
 Write-Host  -ForegroundColor Green "Restarting in 10 seconds!"
