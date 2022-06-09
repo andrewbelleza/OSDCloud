a354cda85f91f04d1f9209f4008a9da8d12f7960
andrewbelleza <54964125+andrewbelleza@users.noreply.github.com>
2022-06-10 03:16:29 +0800

    Update Win10-Test.ps1

--- Profiles/Win10-Test.ps1
+++ Profiles/Win10-Test.ps1
@@ -7,7 +7,7 @@ if ((Get-MyComputerModel) -match 'Virtual') {
 }
 
 Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
-Install-Module OSD -Force
+Install-Module OSD -Force -SkipPublisherCheck
 
 Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
 Import-Module OSD -Force   
